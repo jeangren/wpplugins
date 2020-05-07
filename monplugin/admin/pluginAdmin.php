314b1357875f757d7a3b592a31342e92e76d7bc4
Jean Grenouiller <jeangren@free.fr>
2020-05-07 11:48:57 +0200

    plugin ok

---
+++ monplugin/admin/pluginAdmin.php
@@ -0,0 +1,4 @@
+<?php
+
+echo 'tout va bien';
+
--- monplugin/monplugin.php
+++ monplugin/monplugin.php
@@ -1,23 +1,36 @@
-<!-- version travail -->
+
 <?php
-// plugin Name: Monplugin
+// plugin Name: monplugin
 
-function activatehook()
-{
-    error_log('active');
+// function activatehook()
+// {
+//     error_log('active');
+// }
+// register_activation_hook( __FILE__, 'activatehook' ) ;
+
+
+// function desactivehook()
+// {
+//     error_log('desactive');
+// }
+// register_deactivation_hook( __FILE__, 'desactivehook' ) ;
+
+
+// function uninstallhook()
+// {
+//     error_log('uninstallhook');
+// }
+// register_uninstall_hook( __FILE__, 'uninstallhook' ) ;
+function monplugin_addmenu(){
+    error_log("ajout de menu...");
+    add_menu_page(
+        'Mon plugin admin',
+        'Menu',
+        'manage_options',
+        plugin_dir_path( __FILE__ ) . 'admin/pluginAdmin.php',
+        null,
+        'dashicons-admin-tools',
+        20
+    );
 }
-register_activation_hook( __FILE__, 'activatehook' ) ;
-
-
-function desactivehook()
-{
-    error_log('desactive');
-}
-register_deactivation_hook( __FILE__, 'desactivehook' ) ;
-
-
-function uninstallhook()
-{
-    error_log('uninstallhook');
-}
-register_uninstall_hook( __FILE__, 'uninstallhook' ) ;
+add_action( 'admin_menu', 'monplugin_addmenu' );
